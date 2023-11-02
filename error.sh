6a883d8d7ebda123df626b81ad7652fdd9996ebf
Himani1519 <44604176+Himani1519@users.noreply.github.com>
2023-11-02 11:47:34 +0530

    Update error.sh

--- error.sh
+++ error.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-message="This is a message"
+
+# This is a simple script to print "Hello, World!"
+
+# Missing quotes around the message variable
+message=Hello, World!
+
+# Attempt to print the message
 echo $message
