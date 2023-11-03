87967dc8af4611a228b77c7595a9eb01879e206f
Himani1519 <44604176+Himani1519@users.noreply.github.com>
2023-11-03 12:08:41 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 /*
- This program and the accompanying materials are
- made available under the terms of the Eclipse Public License v2.0 which accompanies
- this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
 
- SPDX-License-Identifier: EPL-2.0
+  SPDX-License-Identifier: EPL-2.0
 
- Copyright Contributors to the Zowe Project.
+  Copyright Contributors to the Zowe Project.
 */
 
 module.exports = {
@@ -13,6 +13,22 @@ module.exports = {
     browser: true,
     es6: true,
     node: true
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        caseSensitive: false,
+      },
+    },
+  },
+  globals: {
+    describe: 'readonly',
+    it: 'readonly',
+    before: 'readonly',
+    after: 'readonly',
+    beforeEach: 'readonly',
+    afterEach: 'readonly',
   },
   ignorePatterns: ['.github/**/*.yml', '**/.build', '**/build', '**/dist', '**/node_modules', '**/release', '**/lib','**/test/yaml-loader'],
   extends: [
@@ -25,19 +41,32 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['header'],
   rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    "no-undef": "off",
+//    'no-restricted-imports': [
+//      'warn',
+//      {
+//        patterns: ['*'],
+//        paths: ['*'],
+//      },
+//    ],
+
    'header/header': [
       2,
       'block',
-      [
-        '\n * This program and the accompanying materials are' +
-          '\n * made available under the terms of the Eclipse Public License v2.0 which accompanies' +
-          '\n * this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html' +
-          '\n *' +
-          '\n * SPDX-License-Identifier: EPL-2.0' +
-          '\n *' +
-          '\n * Copyright Contributors to the Zowe Project.' +
-          '\n '
-      ],
+          [
+        '/*',
+        '  This program and the accompanying materials are',
+        '  made available under the terms of the Eclipse Public License v2.0 which accompanies',
+        '  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html',
+        '  ',
+        '  SPDX-License-Identifier: EPL-2.0',
+        '  ',
+        '  Copyright Contributors to the Zowe Project.',
+        '*/',
+        '',
+      ].join('\n'),
       2
     ]
   }
