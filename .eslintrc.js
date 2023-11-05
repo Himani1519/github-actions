2382017be63c80a11406becc8656b99adc0c0db6
Himani1519 <44604176+Himani1519@users.noreply.github.com>
2023-11-05 12:12:13 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,7 @@ module.exports = {
   rules: {
     // ...other ESLint rules...
 
-    'custom/header-check': 'error', // Enable the custom rule
-  },
-
-  overrides: [
-    {
-      files: ['*.js'], // Target JavaScript files
-      rules: {
-        'custom/header-check': 'error', // Enable the custom rule for these files
-      },
-    },
-  ],
-
-  plugins: ['custom'], // Enable your custom rules
-  settings: {
-    custom: {
-      headerPattern: /\/\*\*\n([\s\*]*[^*]+)*\n([\s\*]*\*\/\n){1}[\s\*]*SPDX-License-Identifier: EPL-2\.0\n[\s\*]*Copyright Contributors to the Zowe Project\.\n\*\//,
-    },
+    // Enforce a specific header format using a custom rule
+    'header/header-format': ['error', '/\\*\\*\\s+This program and the accompanying materials are\\n\\s+made available under the terms of the Eclipse Public License v2.0 which accompanies\\n\\s+this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html\\n\\n\\s+SPDX-License-Identifier: EPL-2.0\\n\\n\\s+Copyright Contributors to the Zowe Project\\.\\s+\\*/'],
   },
 };
