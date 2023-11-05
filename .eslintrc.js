6c0bccade93d218775b54eea1127e6129d592d11
Himani1519 <44604176+Himani1519@users.noreply.github.com>
2023-11-05 12:59:13 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 /*
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
-  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html  
 */
 
 module.exports = {
@@ -26,7 +25,7 @@ module.exports = {
       2,         // Error level
       'block',   // Location
      [
-        '\n  This program and the accompanying materials are\n  made available under the terms of the Eclipse Public License v2.0 which accompanies\n  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html'
+        '\n  This program and the accompanying materials are\n  made available under the terms of the Eclipse Public License v2.0 which accompanies\n'
      ],
       2,        // Comments
     ],
