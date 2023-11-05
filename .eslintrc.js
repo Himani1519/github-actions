14a85d58097287b7d8dd2551b6ff6a5a7f4297e8
Himani1519 <44604176+Himani1519@users.noreply.github.com>
2023-11-05 12:17:32 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,37 @@
+/*
+ This program and the accompanying materials are made available under the terms of the
+ Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ https://www.eclipse.org/legal/epl-v20.html
+
+ SPDX-License-Identifier: EPL-2.0
+ 
+ Copyright Contributors to the Zowe Project.
+ */
+
 module.exports = {
-  // ...other ESLint configuration...
-
-  rules: {
-    // ...other ESLint rules...
-
-    // Enforce a specific header format using a custom rule
-    'header/header-format': ['error', '/\\*\\*\\s+This program and the accompanying materials are\\n\\s+made available under the terms of the Eclipse Public License v2.0 which accompanies\\n\\s+this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html\\n\\n\\s+SPDX-License-Identifier: EPL-2.0\\n\\n\\s+Copyright Contributors to the Zowe Project\\.\\s+\\*/'],
+  env: {
+    browser: true,
+    es6: true,
+    node: true
   },
-};
+  ignorePatterns: ['.github/**/*.yml', '**/.build', '**/build', '**/dist', '**/node_modules', '**/release', '**/lib'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/electron',
+    'plugin:import/typescript'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['header'],
+   rules: {
+    'header/header-format': [
+      'error',
+      {
+        pattern: /^\s*\/\*{2}\n(?:\s+\w+[^\n]*\n)+\s+\*\/\n\s*SPDX-License-Identifier: EPL-2\.0\n\s*Copyright Contributors to the Zowe Project\.\n\s*\*\/$/,
+        comments: 2,  // The number of comments to check
+      },
+    ],
+  },
+}
